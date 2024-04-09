ffa85eecf57b0d6cc5b02d6db67f76c8bb45d884
Yasset Perez-Riverol <ypriverol@gmail.com>
2024-04-09 16:59:09 +0100

    First refactoring of workflow. (6)

--- bin/vector_search_table.py
+++ bin/vector_search_table.py
@@ -24,7 +24,6 @@ def resolve_file_loc(root_directory: str, file_pattern: str) -> Path:
 
 def extract_mgf_path_info(usi_str: str) -> str:
     """
-    提取mgf_path信息，以聚类结果文件为标准
     :param usi_str:
     :return:
     """
@@ -38,7 +37,6 @@ def extract_mgf_path_info(usi_str: str) -> str:
 
 def extract_cluster_tsv(cluster_res_path: str, pxd_folder: str):
     """
-    复合簇和独立簇要分开提取信息吗，检索然后删除复合簇中大于10个的psm才会进行判断
     :param pxd_folder: quantmsio得到的目录
     :param cluster_res_path:聚类结果路径
     :return:
@@ -51,9 +49,8 @@ def extract_cluster_tsv(cluster_res_path: str, pxd_folder: str):
 
     cluster_res_df = pd.read_csv(cluster_res_path, sep='\t', header=None)
     cluster_res_df.columns = ['mgf_path', 'index', 'cluster_accession']
-    cluster_res_df.dropna(axis=0, inplace=True)  # 删除空行
+    cluster_res_df.dropna(axis=0, inplace=True)
 
-    # 提取出mgf文件的名字,根据这个去找对应的parquet文件
     cluster_res_df['parquet_path'] = (
         cluster_res_df['mgf_path'].apply(
             lambda x: resolve_file_loc(pxd_folder, Path(x).parts[-1].split('_')[0] + ".parquet")))
@@ -62,10 +59,8 @@ def extract_cluster_tsv(cluster_res_path: str, pxd_folder: str):
         group_parquet = pd.read_parquet(path_path_str, columns=parquet_col)
         group_index = group['index'].to_list()
 
-        # 从quantmsio提取'posterior_error_probability', 'global_qvalue'
         group[columns_to_filter] = group_parquet.loc[group_index, columns_to_filter].reset_index(drop=True)
 
-        # 物种和仪器
         group[specie_instrument] = (group['mgf_path'].apply(lambda x: extract_mgf_path_info(x))
                                     .str.split(',', expand=True))
 
@@ -89,14 +84,12 @@ def extract_cluster_tsv(cluster_res_path: str, pxd_folder: str):
 
 
 if __name__ == '__main__':
-    #  mgf文件
     mgf_path = (r'G:\graduation_project\generate-spectrum-library\project_folder'
                 r'\Homo sapiens\TQ Orbitrap XL\charge5\mgf_files\PXD002179-charge5_1.mgf')
     cluster_tsv_path = (r'G:\graduation_project\generate-spectrum-library\project_folder\res_output_folder'
                         r'\files_list_3\maracluster_output\MaRaCluster.clusters_p30.tsv')
     pxd_folder_path = r'G:\graduation_project\generate-spectrum-library\PXD002179\mztab'
 
-    # MaRaCluster结果文件, 默认选取_p30.tsv
     res_df = extract_cluster_tsv(cluster_tsv_path, pxd_folder_path)
     print(res_df.head())
     # res_df.to_csv(r"C:\Users\ASUS\Desktop\test_parquet.csv", index=False)
